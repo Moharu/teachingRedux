c0c55eb9e9a00af8841c84f772d6fd054c0772cf
Joao Telo <joaotelo@Desenvs-MacBook-Air.local>
2017-05-26 10:39:39 -0300

    2

--- index.js
+++ index.js
@@ -1,28 +1,49 @@
-/*
-    Vamos ver um pouco mais dos conceitos:
-    AÇOES:
-
-    Como dito anteriormente, as ações são objetos plenos que vão descrever como
-    o estado será alterado. Elas sempre vão ter uma propriedade 'TYPE', que ensina
-    ao redutor como resolver uma ação daquele tipo. Todas as demais propriedades são
-    escolhidas à critério do programador.
-
-    {
-        type: 'ADD_TODO',
-        text: 'Criar açoes legais'
-    }
-
-    Para que as ações tenham um formato padrão e sempre sejam entendidas pelo nosso redutor,
-    criamos funções chamadas de ACTION CREATORS.
-
-*/
-
 const addTodo = (text) => ({
     type: 'ADD_TODO',
     text
 })
 
+const toggleTodo = (index) => ({
+    type: 'TOGGLE_TODO',
+    index
+})
+
 /*
-    Gostaria também de ter uma ação para marcar um TODO como
-    completo/incompleto, como seria o action creator dela?
+    Já temos nossos ACTION CREATORS, agora precisamos
+    imaginar qual vai ser o formato do nosso estado.
+
+    Como ele é uma lista de TODOs, ele pode ser simplesmente
+    um array com objetos que possuem a informação do texto e se
+    o TODO foi completo.
+
+    Portanto, o estado inicial do nosso APP é na verdade um array vazio
 */
+
+const initialState = []
+
+/*
+    Agora vamos escrever a função redutora que vai dizer como o estado da
+    aplicação vai responder a uma ação em particular.
+
+    Abaixo temos o redutor apresentado anteriormente, você consegue
+    implementar o caso do 'TOGGLE_TODO'?
+    LEMBRE-SE: NÃO DEVEMOS ALTERAR O ESTADO ANTERIOR
+*/
+
+const todoApp = (state = initialState, action = {}) => {
+    switch(action.type){
+        case 'ADD_TODO':
+            return [...state, { text: action.text, completed: false}]
+        case 'TOGGLE_TODO':
+            return state // ???
+        default:
+            return state
+    }
+}
+
+let state = todoApp() // inicializando com o estado DEFAULT
+state = todoApp(state, addTodo('plantar uma arvore'))
+state = todoApp(state, addTodo('doar sangue'))
+console.log('antes: ', state)
+// Alterar o estado de 'doar sangue' pra COMPLETED
+console.log('depois: ', state)
